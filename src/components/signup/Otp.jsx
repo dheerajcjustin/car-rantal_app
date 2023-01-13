d5062fb7ceca98e4134d32beaf43bd2fe8d0e0f8
dheerajcjustin <dheerajcjustin@gmail.com>
2023-01-13 17:42:50 +0530

    otp verication

--- src/components/signup/Otp.jsx
+++ src/components/signup/Otp.jsx
@@ -9,18 +9,33 @@ const Otp = ({ userData }) => {
   const loginHandle = () => {
     navigate("/login");
   };
+  const otpResendHandler = () => {
+    console.log(userData);
+  };
   const [otp, setOtp] = useState("");
   const handleChange = (enteredOtp) => {
     setOtp(enteredOtp);
   };
-
+  const [error, setError] = useState(false);
   const otpSentButtonHandler = () => {
-    console.log("it is working with otp", otp);
+    if (otp.length < 4) {
+      console.log("invalid otp");
+      setError(true);
+    } else {
+      setError(false);
+      console.log("it is working with otp", otp);
+      navigate(-1);
+    }
   };
   return (
     <div className="md:col-span-2 lg:col-span-1 flex flex-col items-center justify-center bg-[#FDD23F]">
-      <h1 className="font-Viaoda text-7xl mb-10"> Mobile Verification</h1>
-      <p>otp has sent to {userData.phone}</p>
+      <h1 className="font-Viaoda text-5xl mb-10"> Mobile Verification</h1>
+      <p className="p-10 text-xl">
+        otp has sent to {userData.phone}
+        <button className="bg-gray-50 rounded-xl w-32 h-16  ml-5">
+          change Mobile
+        </button>
+      </p>
 
       <OtpInput
         value={otp}
@@ -30,6 +45,13 @@ const Otp = ({ userData }) => {
         numInputs={4}
         isInputNum={true}
       ></OtpInput>
+      {error && <p className="text-red-600 text-2xl">invalid otp</p>}
+      <p
+        className="text-xl text-right w-[65%] cursor-pointer underline"
+        onClick={otpResendHandler}
+      >
+        Resend Otp ?
+      </p>
       <button
         onClick={otpSentButtonHandler}
         className="w-[60%] h-20 mt-10 text-3xl font-semibold border-2 border-black rounded-3xl text-center hover:scale-105 hover:bg-black hover:text-white"
