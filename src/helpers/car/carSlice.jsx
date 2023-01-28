64fe551ad0ffeba6c5801936806b6eb2f94fae53
dheerajcjustin <dheerajcjustin@gmail.com>
2023-01-28 14:37:12 +0530

    car slice

---
+++ src/helpers/car/carSlice.jsx
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const carSlice = createSlice({
+  name: "car",
+  initialState: { car: {}, time: {} },
+  reducers: {
+    setCarData: (state, action) => {
+      // console.log(
+      //   "wwowo inside the set car data and the payload is ",
+      //   action.payload
+      // );
+      const { car, time } = action.payload;
+      state.car = car;
+      state.time = time;
+    },
+  },
+});
+
+export const { setCarData } = carSlice.actions;
+
+export default carSlice.reducer;
+export const selectCurrentCar = (state) => state.car.car;
+export const selectCarBookingTime = (state) => state.car.time;
