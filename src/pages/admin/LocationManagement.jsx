b25b4e68576d537c00e7967cce782215d92e2b48
dheerajcjustin <dheerajcjustin@gmail.com>
2023-02-02 12:28:30 +0530

    map adding

--- src/pages/admin/LocationManagement.jsx
+++ src/pages/admin/LocationManagement.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import Sidebar from "../../components/admin/Sidebar";
-import AddLocation from "../../components/admin/AddLocation";
+import AddLocation from "../../components/admin/location/AddLocation";
+import AddPickUP from "../../components/admin/location/AddPickup"
 import LocationTable from "../../components/admin/LocationTable";
 import Modal from "../../components/UI/Modal";
-import authInstance from "../../config/authInstance";
+import { fetcher } from "../../config/authInstance";
+
 import useSWR from "swr";
+// import PickupMap from "../../components/admin/location/PickupMap";
+import AddPickup from "../../components/admin/location/AddPickup";
+import PickupPoints from "../../components/admin/location/PickupPoints";
 
 const LocationManagement = () => {
+
+  const [pickup, setPickup] = useState({});
+  const [showLocation, setShowLocation] = useState(true);
   const [locationModal, setLocationModal] = useState(false);
   const modalCloseHandler = () => {
     setLocationModal(false);
@@ -14,7 +22,6 @@ const LocationManagement = () => {
   const modalOpenHandler = () => {
     setLocationModal(true);
   };
-  const fetcher = (url) => authInstance.get(url).then((res) => res.data);
   // .catch((err) => console.log(err));
   const { data, error, isLoading, mutate } = useSWR(`/admin/location`, fetcher);
   return (
@@ -22,26 +29,35 @@ const LocationManagement = () => {
       <Sidebar type="location" />
       <div className="w-full h-screen">
         <div className="max-w-[90%] mx-auto bg-white mt-20 rounded-3xl p-8">
-          <div className="flex flex-col w-full gap-2">
-            {locationModal && (
-              <Modal onClose={modalCloseHandler}>
-                <AddLocation onClose={modalCloseHandler} mutate={mutate} />
-              </Modal>
-            )}
-            <button
-              className="bg-[#FDD23F] max-w-xs  h-14 rounded-lg font-semibold"
-              onClick={modalOpenHandler}
-            >
-              Add Location
-            </button>
-            <LocationTable
-              data={data}
-              isLoading={isLoading}
-              error={error}
-              mutate={mutate}
-            />
-          </div>
+          {showLocation ?
+            <div className="flex flex-col w-full gap-2">
+              <button
+                className="bg-[#FDD23F] max-w-xs  h-14 rounded-lg font-semibold"
+                onClick={modalOpenHandler}
+              >
+                Add Location
+              </button>
+              {locationModal && (
+                <Modal onClose={modalCloseHandler}>
+                  <AddLocation onClose={modalCloseHandler} mutate={mutate} />
+                </Modal>
+              )}
+              <LocationTable
+                setPickup={setPickup}
+                setShowLocation={setShowLocation}
+                data={data}
+                isLoading={isLoading}
+                error={error}
+                mutate={mutate}
+
+              />
+            </div> :
+            <PickupPoints pickup={pickup} backButton={setShowLocation} />
+          }
+
         </div>
+        {/* <AddLocation /> */}
+
       </div>
     </div>
   );
